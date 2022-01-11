605e6e860c4e65e450637fb5c2c3da01a18e9a78
moazamoshtha <mama.moshtha@gmail.com>
2022-01-11 20:57:58 +0200

    finish auth [login,logout] with generate api-token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $fields = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($request->userType == 'admin'){
+            $user = Admin::where('username',$fields['username'])->first();
+        }else{
+            $user = User::where('username',$fields['username'])->first();
+        }
+
+        if (!$user){
+            return response([
+                'status' => false,
+                'message' => 'المستخدم غير موجود'
+            ],401);
+        }
+
+        if (!$user || !Hash::check($fields['password'],$user->password)){
+            return response([
+                'status' => false,
+                'message' => 'خطأ في كلمة السر'
+            ],401);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $data = [
+            'status' => true,
+            'message' =>'تم تسجيل الدخول بنجاح',
+            'data' => [
+                'user' => $user,
+                'token' => $token,
+            ]
+        ];
+
+        return response($data,201);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response([
+                'status' => true,
+                'message' => 'تم تسجيل الخروج'
+            ]
+        );
+    }
+}
