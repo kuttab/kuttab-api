1d14783e4a83723f3043f25f155ce012bcb1490c
moazamoshtha <mama.moshtha@gmail.com>
2022-01-14 17:08:05 +0200

    add function getChildren with daily records

--- app/Http/Controllers/Api/ParentChildController.php
+++ app/Http/Controllers/Api/ParentChildController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\DailyRecord;
+use App\Models\ParentChild;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Validator;
 
 class ParentChildController extends Controller
 {
@@ -14,7 +18,7 @@ class ParentChildController extends Controller
      */
     public function index()
     {
-        //
+        return ParentChild::all();
     }
 
     /**
@@ -25,7 +29,28 @@ class ParentChildController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'school_id' => 'required|exists:schools,id',
+            'parent_id' => 'required|exists:users,id',
+            'child_id' => 'required|exists:users,id',
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()->first()
+            ],422);
+        }
+
+        $parentChild = ParentChild::create($request->all());
+
+        $data = [
+            'status' => true,
+            'message' => 'تم ادراج طفل جديد',
+            'data' => $parentChild,
+        ];
+
+        return response()->json($data,201);
     }
 
     /**
@@ -34,9 +59,21 @@ class ParentChildController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request,$id)
     {
-        //
+        //$id = parent_id
+        $parentChild = ParentChild::where('parent_id',$id)->where('child_id',$request->child_id)->first();
+
+        if (is_null($parentChild)){
+            return response()->json([
+                'status' => false,
+                'message' => 'سجل غير موجود',
+            ]);
+        }
+
+        $parentChild->parent;
+        $parentChild->child;
+        return $parentChild;
     }
 
     /**
@@ -57,8 +94,42 @@ class ParentChildController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request,$id)
     {
-        //
+        $parentChild = ParentChild::where('parent_id',$id)->where('child_id',$request->child_id)->first();
+
+        if (is_null($parentChild)){
+            return response()->json([
+                'status' => false,
+                'message' => 'سجل غير موجود',
+            ]);
+        }
+
+        $parentChild->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'تم حذف السجل',
+        ]);
     }
+
+    public function getChildren($id){
+        $parentChildren = ParentChild::where('parent_id',$id)->with(['child'])->get();
+        $children = [];
+        foreach ($parentChildren as $parentChild){
+            array_push($children,$parentChild->child) ;
+        }
+
+        return $children;
+    }
+
+    public function lastRecordForEachChild($id){
+        $parentChildren = ParentChild::where('parent_id',$id)->with(['child.lastDailyRecord.quraan'])->get();
+        $childrenWithLastRecord = [];
+        foreach ($parentChildren as $parentChild){
+            array_push($childrenWithLastRecord,$parentChild->child) ;
+        }
+
+        return $childrenWithLastRecord;
+    }
+
 }
