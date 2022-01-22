577b50feee682fb1e84b5cd398fe3fb9ad95cbf7
moazamoshtha <mama.moshtha@gmail.com>
2022-01-22 21:44:40 +0200

    add getSchoolId as golbal function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,16 +12,12 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    protected $school_id;
-
-    public function __construct()
-    {
-        $this->school_id = auth('sanctum')->user()->school_id;
-        View::share('school_id', $this->school_id);
-    }
-
     function randomPassword() {
         $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         return substr(str_shuffle($chars),0,8);
+    }
+
+    function getSchoolId(){
+        return auth('sanctum')->user()->school_id;
     }
 }
