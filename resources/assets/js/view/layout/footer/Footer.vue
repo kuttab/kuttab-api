6d0913c6ab75cf775f876a633d422918bcd21b75
Moaz A.Moshtha <mama.moshtha@gmail.com>
2022-03-07 23:31:57 +0200

    delete unused links

--- resources/assets/js/view/layout/footer/Footer.vue
+++ resources/assets/js/view/layout/footer/Footer.vue
@@ -2,42 +2,15 @@
   <!-- begin:: Footer -->
   <div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
     <div
-      class="d-flex align-items-center justify-content-between"
+      class="d-flex align-items-center justify-content-end"
       v-bind:class="{ 'container-fluid': widthFluid, container: !widthFluid }"
     >
       <div class="text-dark">
-        <span class="text-muted font-weight-bold mr-2">
-          2021 &nbsp;&copy;&nbsp;
+        <span class="text-muted font-weight-bold ">
+          2022 &copy;
         </span>
-        <a
-          href="http://keenthemes.com/metronic"
-          target="_blank"
-          class="text-dark-75 text-hover-primary"
-        >
+        <a class="text-dark-75 text-hover-primary">
           Kuttab
-        </a>
-      </div>
-      <div class="nav nav-dark">
-        <a
-          href="http://keenthemes.com/metronic"
-          target="_blank"
-          class="nav-link pr-3 pl-0"
-        >
-          About
-        </a>
-        <a
-          href="http://keenthemes.com/metronic"
-          target="_blank"
-          class="nav-link px-3"
-        >
-          Team
-        </a>
-        <a
-          href="http://keenthemes.com/metronic"
-          target="_blank"
-          class="nav-link pl-3 pr-0"
-        >
-          Contact
         </a>
       </div>
     </div>
