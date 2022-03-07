0e63c6ff785dcb4b7ce3c3a9f89aa72297cdd986
Moaz A.Moshtha <mama.moshtha@gmail.com>
2022-03-08 00:18:52 +0200

    get family and show all children

--- resources/assets/js/view/pages/families/Show.vue
+++ resources/assets/js/view/pages/families/Show.vue
@@ -5,7 +5,7 @@
             <!--begin::Row-->
             <div class="row">
                 <!--begin::Col-->
-                <div v-for="user in parents" class="col-xl-4 col-lg-6 col-md-6 col-sm-6">
+                <div v-for="user in users" class="col-xl-4 col-lg-6 col-md-6 col-sm-6">
                     <!--begin::Card-->
                     <div class="card card-custom gutter-b card-stretch">
                         <!--begin::Body-->
@@ -17,11 +17,8 @@
                                     <!--begin::Pic-->
                                     <div class="flex-shrink-0 mr-4 mt-lg-0 mt-3">
                                         <div class="symbol symbol-circle symbol-lg-75">
-                                            <img v-if="user.image" :src="'./storage/'+user.image" alt="image">
+                                            <img v-if="user.image" :src="'../storage/'+user.image" alt="image">
                                             <img v-else :src="'./media/users/default.jpg'" alt="image">
-                                        </div>
-                                        <div class="symbol symbol-lg-75 symbol-circle symbol-primary d-none">
-                                            <span class="font-size-h3 font-weight-boldest">JM</span>
                                         </div>
                                     </div>
                                     <!--end::Pic-->
@@ -43,29 +40,31 @@
                             <!--begin::Info-->
                             <div class="mb-7">
                                 <div class="d-flex justify-content-center align-items-center">
-                                    <h5 class="text-hover-primary mb-2"></h5>
+                                    <h5 class="text-hover-primary mb-2">{{user.first_name+' '+user.middle_name+' '+user.last_name}}</h5>
                                 </div>
                                 <div class="d-flex justify-content-between align-items-center my-1">
-                                    <span class="text-dark-75 font-weight-bolder mr-2">{{$t('PARENT_CHILD.ADD.NAV.GENERAL_INFO.FROM.PARENT')}}:</span>
-                                    <a class="text-muted text-hover-primary">{{user.first_name+' '+user.middle_name+' '+user.last_name}}</a>
+                                    <span class="text-dark-75 font-weight-bolder mr-2">{{$t('USERS.ADD.NAV.ADDRESS_INFO.FROM.ADDRESS')}}:</span>
+                                    <a class="text-muted text-hover-primary">{{ user.address }}</a>
                                 </div>
                                 <div class="d-flex justify-content-between align-items-center my-1">
                                     <span class="text-dark-75 font-weight-bolder mr-2">{{$t('USERS.ADD.NAV.CONTACT_INFO.FROM.MOBILE')}}:</span>
                                     <a class="text-muted text-hover-primary">{{ user.mobile_number }}</a>
                                 </div>
                                 <div class="d-flex justify-content-between align-items-center my-1">
-                                    <span class="text-dark-75 font-weight-bolder mr-2">{{$t('PARENT_CHILD.ADD.NAV.GENERAL_INFO.FROM.CHILDREN')}}:</span>
+                                    <span class="text-dark-75 font-weight-bolder mr-2">{{$t('USERS.ADD.NAV.PERSONAL_INFO.FROM.ACADEMIC')}}:</span>
+                                    <a class="text-muted text-hover-primary">{{ user.academic }}</a>
                                 </div>
-                                <div class="row d-flex justify-content-center align-items-center">
-                                    <a v-for="children in user.children" class="col-12 text-center text-muted text-hover-primary">{{ children.child.first_name + ' ' + children.child.last_name}}</a>
+                                <div class="d-flex justify-content-between align-items-center my-1">
+                                    <span class="text-dark-75 font-weight-bolder mr-2">{{$t('USERS.ADD.NAV.ACCOUNT_INFO.FROM.USERNAME')}}:</span>
+                                    <a class="text-muted text-hover-primary">{{ user.username }}</a>
                                 </div>
                             </div>
                             <!--end::Info-->
                             <div class="mt-9 text-center">
-                                <router-link :to="{name:'show-family',params:{id:user.id}}" class="btn btn-md btn-icon btn-light-success btn-pill mx-2">
-                                    <i class="flaticon-arrows"></i>
+                                <router-link :to="{name:'show-user',params:{id:user.id}}" class="btn btn-md btn-icon btn-light-warning btn-pill mx-2">
+                                    <i class="flaticon-interface-1"></i>
                                 </router-link>
-                                <button @click="showConfirmToast(user.id)" class="btn btn-md btn-icon btn-light-danger btn-pill mx-2">
+                                <button disabled @click="destroy(user.id)" class="btn btn-md btn-icon btn-light-danger btn-pill mx-2">
                                     <i class="flaticon-delete"></i>
                                 </button>
                             </div>
@@ -83,59 +82,34 @@
 </template>
 
 <script>
-import {SET_BREADCRUMB} from "../../store/breadcrumbs.module";
-import apiService from "../../services/api.service";
-import {SET_ACTION_BUTTON_CONFIG} from "../../store/config.module";
-import KTUtil from "../../helper/util";
-import formValidation from "../../plugins/formvalidation/dist/es6/core/Core";
-import Trigger from "../../plugins/formvalidation/dist/es6/plugins/Trigger";
-import SubmitButton from "../../plugins/formvalidation/dist/es6/plugins/SubmitButton";
-import Bootstrap from "../../plugins/formvalidation/dist/es6/plugins/Bootstrap";
-import Swal from "sweetalert2";
-import toastConfirm from "./vue-toast/toast-confirm";
-let baseApi = 'api/v1/parentChild/'
-
+import {SET_BREADCRUMB} from "../../../store/breadcrumbs.module";
+import apiService from "../../../services/api.service";
+let baseApi = ''
 export default {
-    name: "Families",
+    name: "Show",
     data() {
         return {
-            parents: []
+            users: []
         }
     },
     mounted() {
-        this.$store.dispatch(SET_BREADCRUMB, [{ title: this.$t('PARENT_CHILD.TITLE') }]);
-        this.$store.dispatch(SET_ACTION_BUTTON_CONFIG, { display: true,title:this.$t('MENU.NEW'),route:'/families/add' });
-        this.index();
+        this.$store.dispatch(SET_BREADCRUMB, [
+            {title: this.$t('PARENT_CHILD.TITLE'), route: "families"},
+            {title: this.$t('PARENT_CHILD.SHOW.TITLE')},
+        ]);
+        this.show(this.$route.params.id);
     },
     methods: {
-        index() {
-            apiService.get('api/v1/parentChild').then(({data}) => this.parents = data)
+        show(id) {
+            apiService.get('api/v1/parent/'+id+'/children').then(({data}) =>{
+                this.users = data
+            })
         },
         destroy(id) {
-            apiService.delete('api/v1/parentChild/' + id).then(() => {
-                this.$toast.success(this.$t('PARENT_CHILD.API.RESPONSE.MESSAGE.DELETE'))
+            apiService.delete(baseApi + id).then(() => {
+                this.$toast.success(this.$t('USERS.API.RESPONSE.MESSAGE.DELETE'))
+                this.index()
             });
-        },
-        showConfirmToast(id) {
-            // Define the content object with the component, props and listeners
-            const content = {
-                component: toastConfirm,
-                // Any prop can be passed, but don't expect them to be reactive
-                props: {
-                    message : 'حذف عائلة !! هل أنت متأكد ؟',
-                },
-                // Listen and react to events using callbacks. In this case we listen for
-                // the "click" event emitted when clicking the toast button
-                listeners: {
-                    click: () => {
-                        this.destroy(id)
-                        this.index()
-                    }
-                }
-            }
-
-            // Render the toast and its contents
-            this.$toast(content);
         }
     }
 }
